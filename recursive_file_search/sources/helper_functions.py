dc932e86f6de1d07aefe23da380b2f470d26ff37
Mary <marydrobotun@gmail.com>
2025-02-19 00:51:19 +0300

    new helpers

--- recursive_file_search/sources/helper_functions.py
+++ recursive_file_search/sources/helper_functions.py
@@ -11,6 +11,10 @@ def has_every_vowel(path: str):
     return ('a' in name) and ('e' in name) and ('i' in name) and ('o' in name) and ('u' in name)
 
 
-def is_size_more_than(path: str, maxsize: int = 1000):
+def is_size_more_than(path: str, minsize: int = 1000):
+    filesize = os.path.getsize(path)
+    return filesize > minsize
+
+def is_size_less_than(path: str, maxsize: int = 1000):
     filesize = os.path.getsize(path)
     return filesize > maxsize
